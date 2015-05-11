17620e3bf98b63d9515dbdcb425c688296f5b93e
Jacob M <jmeixner@uwm.edu>
2015-05-11 10:50:47 -0500

    Added some new images and updated the code
    The code now uses the covarient matrix as its principal way to compute
    eigen faces.

--- EigfacesEg.py
+++ EigfacesEg.py
@@ -10,54 +10,60 @@ import numpy as np
 import numpy.linalg as lin
 
 # total count of faces
-L = 687
+L = 2 #687
 
 # list file directories
-basedir = 'D:\\Users\\jmeixner\\Documents\\SchoolWork\\fas'
-flist = os.listdir(basedir)
+basedir = 'D:\\Users\\jmeixner\\Documents\\SchoolWork\\eigFaces\\'
+facesdir = basedir + 'fas\\'
+flist = os.listdir(facesdir)
 
 # create zero array
-arr = np.zeros((L, 512))
+arr = np.zeros((L, 43621))
 
-num = 0
+num = -1
 
 # load data matrix out of PGM files
 for f in flist:
+  num += 1
   # construct target
-  tfile = basedir + str(f)
+  tfile = facesdir + str(f)
 
-  #try:
-    print "Opening:", tfile
-    l = misc.lena()
-    im = misc.imread(basedir + f)
-    
-    print "Storing image in memory matrix"
-    for i in xrange(im.size):
-      pix = np.zeros(262144,1) 
-      for j in xrange(512):
-        pix += im[i]
-        end
-      print pix
-      print type(pix)
-      arr[i,:] = pix
-      num += 1
- # except :
-  #  pass
+  print "Opening:", tfile
+  l = misc.lena()
+  im = misc.imread(tfile)
 
-arr = arr / 256
+  #graying out the image
+  gray = np.zeros((im.shape[0],im.shape[1]))
+  for i in xrange(im.shape[0]):
+    for j in xrange(im.shape[1]):
+      gray[i,j] = (im[i,j,0] + im[i,j,1] + im[i,j,2]) / 3
+      
+  #scipy.misc.imsave(basedir + 'tmp\\' + str(f) + '_gray.png',gray)
 
-print arr
+  print "Storing image in memory matrix"
+  for i in xrange(im.shape[0]):
+    for j in xrange(im.shape[1]):
+      arr[num,(j) + i * im.shape[1]] = gray[i,j]
 
 # subtract the mean image from each image sample
-for i in xrange(arr.shape[0]):
-  arr[i,:] = arr[i,:] - arr.mean(0)
+for i in xrange(arr.shape[1]):
+  arr[:,i] = arr[:,i] - np.mean(arr,1)
 
-# compute SVD of the data matrix
-print "Computing sparse SVD of data matrix"
-U, V, T = lin.svd(arr.transpose(), full_matrices=False)
+S = np.cov(np.transpose(arr))
+
+# V here are the eigenvecotrs of the covarience matrix
+# this makes them the eigen faces
+e, V = lin.eig(S)
+
+
+    # compute SVD of the data matrix
+    #print "Computing sparse SVD of data matrix"
+    #U, V, T = lin.svd(arr.transpose(), full_matrices=True)
 
 # print eigenfaces to files
 print "Writing eigenvectors to disk..."
 for i in xrange(L):
-  scipy.misc.imsave(basedir + str(i) + '.png', U[:,i].reshape(192,128))
+  print i
+  scipy.misc.imsave(basedir + 'eigFaces\\' + str(i) + '.png', U[:,i].reshape(U.shape[0],1))
+
 
